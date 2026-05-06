73e01411908c6f2887544040d03373f3667d6dad
Ross Addison <rossaddison@users.noreply.github.com>
2026-05-06 20:51:06 +0100

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,8 +7,7 @@ namespace Yiisoft\Yii\Gii\Controller;
 use Psr\Http\Message\ResponseInterface;
 use ReflectionClass;
 use ReflectionParameter;
-use Yiisoft\DataResponse\DataResponse;
-use Yiisoft\DataResponse\DataResponseFactoryInterface;
+use Yiisoft\DataResponse\ResponseFactory\DataResponseFactoryInterface;
 use Yiisoft\Http\Status;
 use Yiisoft\Input\Http\Attribute\Parameter\Query;
 use Yiisoft\Validator\Helper\RulesDumper;
@@ -141,7 +140,7 @@ final class DefaultController
         );
     }
 
-    private function createErrorResponse(InvalidGeneratorCommandException $e): DataResponse
+    private function createErrorResponse(InvalidGeneratorCommandException $e): ResponseInterface
     {
         return $this->responseFactory->createResponse(
             ['errors' => $e->getResult()->getErrorMessagesIndexedByProperty()],
